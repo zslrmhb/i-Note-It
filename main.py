d3faa50aedaa66879f26af8970bdba0da0befda1
Kevin Zhang <jiz100@ucsd.edu>
2023-01-18 23:11:57 -0800

    api works

--- main.py
+++ main.py
@@ -2,27 +2,13 @@ import streamlit as st
 from streamlit_chat import message
 import requests
 
-API_KEY = '1OS8cnPzWvuRAAI1YfLg71UaDevihHbe'        
+# '''API_KEY = '1OS8cnPzWvuRAAI1YfLg71UaDevihHbe'''
 url = "https://api.ai21.com/studio/v1/j1-grande/i-Note-it/complete"
 
 def get_notes(transcript):
-    '''req = requests.post(
-        "https://api.ai21.com/studio/v1/j1-large/complete",
-        headers={f"Authorization": "Bearer {API_KEY}"},
-        json={
-            "prompt": 'transcript', 
-            "numResults": 1, 
-            "maxTokens": 8, 
-            "stopSequences": ["."],
-            "topKReturn": 0,
-            "temperature": 0.2
-        }
-    )
-    
-    return req.json()   # testing'''
     instruction = "Imagine you are the best notetaker in the world. Write the most streamlined and hierarchical bullet point notes with sections for this text:\n"
 
-    
+        
     payload = {
         "prompt": f"{instruction}{transcript}",
         "numResults": 1,
@@ -53,19 +39,17 @@ def get_notes(transcript):
             "applyToStopwords": False,
             "applyToWhitespaces": False,
             "applyToEmojis": False
-      },
+        },
     "stopSequences":[]
     }
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "Authorization": "Bearer {API_KEY}"
+        "Authorization": "Bearer 9LpqTGIFLsKwtjtVHwIwGRLxIeP9tQDT"
     }
-    # print(payload['prompt'])
 
     response = requests.post(url, json=payload, headers=headers)
-    print(response.json())
-    #print(response.json()['completions'][0]['data']['text'])
+    return(response.json()['completions'][0]['data']['text'])
 
 
     
