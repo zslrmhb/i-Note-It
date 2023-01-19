1913fb3d46343c1289ec09523501e6495525d5b7
Kevin Zhang <jiz100@ucsd.edu>
2023-01-18 21:21:53 -0800

    change api call

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 from streamlit_chat import message
 import requests
 
-API_KEY = 'BVO7yd0kvpc2UMo95zY70Jt56E6NnBxY'        
+API_KEY = '1OS8cnPzWvuRAAI1YfLg71UaDevihHbe'        
+url = "https://api.ai21.com/studio/v1/j1-grande/i-Note-it/complete"
 
 def get_notes(transcript):
-    req = requests.post(
+    '''req = requests.post(
         "https://api.ai21.com/studio/v1/j1-large/complete",
         headers={f"Authorization": "Bearer {API_KEY}"},
         json={
@@ -18,7 +19,55 @@ def get_notes(transcript):
         }
     )
     
-    return req.json()   # testing
+    return req.json()   # testing'''
+    instruction = "Imagine you are the best notetaker in the world. Write the most streamlined and hierarchical bullet point notes with sections for this text:\n"
+
+    
+    payload = {
+        "prompt": f"{instruction}{transcript}",
+        "numResults": 1,
+        "maxTokens": 2048,
+        "temperature": 0,
+        "topKReturn": 0,
+        "topP":1,
+        "countPenalty": {
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        "frequencyPenalty": {
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+        },
+        "presencePenalty": {
+            "scale": 0,
+            "applyToNumbers": False,
+            "applyToPunctuations": False,
+            "applyToStopwords": False,
+            "applyToWhitespaces": False,
+            "applyToEmojis": False
+      },
+    "stopSequences":[]
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer {API_KEY}"
+    }
+    # print(payload['prompt'])
+
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.json())
+    #print(response.json()['completions'][0]['data']['text'])
+
+
     
 
 
