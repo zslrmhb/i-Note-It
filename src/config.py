c6478ae2f01bdbc0555ae01c2645fa3dd33b05d3
Hongbin Miao <59853402+zslrmhb@users.noreply.github.com>
2023-01-19 21:42:58 -0800

    Delete config.py

--- src/config.py
+++
@@ -1,3 +0,0 @@
-API_TOKEN = "9LpqTGIFLsKwtjtVHwIwGRLxIeP9tQDT"
-NOTETAKER_MODEL_URL = "https://api.ai21.com/studio/v1/j1-grande/i-Note-it/complete"
-NOTEBOT_MODEL_URL = "https://api.ai21.com/studio/v1/j1-jumbo/complete"
