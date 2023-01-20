c7bf47a57af2ef7d0cf1d9e1d965e33b6ba5a4ec
Hongbin Miao <zslrmhb@gmail.com>
2023-01-19 20:50:51 -0800

    fixed some bugs on note display through streamlit session state

--- src/main.py
+++ src/main.py
@@ -12,6 +12,14 @@ import time
 # Page Config
 st.set_page_config(layout='wide')
 
+# Session States
+if "generated" not in st.session_state:
+    st.session_state["generated"] = False
+    st.session_state["downloaded"] = True
+    st.session_state["asked"] = False
+    st.session_state["most_recent_note"] = ""
+    
+
 # Welcome page
 st.title("i-Note-it")
 st.caption("---Enhance Note-Taking Experience with Artificial Intelligence.")
@@ -25,13 +33,18 @@ NoteBotService = NoteBot()
 col1, col2 = st.columns(2, gap="medium")
 
 with col1:
-    st.header("Input a text")
+    st.header("Your Text")
     generate_button = st.button("Generate")
     text = st.text_area(":keyboard: Input the text ", height=300, max_chars=5000, help="Copy & Paste the text you want to convert into markdown note")  # Corresponds to approximately 2040 tokens (max. tokens for the model)
     
 with col2:
     st.header("Your Note")
+
     if generate_button:
+        st.session_state["generated"] = True
+
+    if st.session_state["generated"]:
+
         generate_success = False
 
         try:
@@ -40,17 +53,20 @@ with col2:
         except KeyError:
             st.error("Try with fewer text!")
 
-
         if generate_success:
             post_processed_note = PostProcessService.process_text(generated_note)
 
             with st.spinner('Wait for it...'):
                 time.sleep(3)
 
-            download_button = st.download_button("Download", post_processed_note, 'i-Note-it.md')
-            st.markdown(post_processed_note)
+            # if st.session_state["downloaded"]:
+            #     download_button = st.download_button("Download", post_processed_note, 'i-Note-it.md')
+            st.session_state["most_recent_note"] = post_processed_note
+            st.session_state["generated"] = False
+    
+    st.markdown(st.session_state["most_recent_note"])
 
-with st.expander("Ask a Bot"):
+with st.expander("Ask your NoteBot!"):
     question = st.text_input("Question")
     if st.button("Ask"):
         message(question, is_user=True)
